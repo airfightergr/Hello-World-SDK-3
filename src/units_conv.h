e7f15ef0347471022a2d14cfecafee2a8c8a4799
ILIAS TSELIOS <ilias@tselios.com>
2023-06-18 01:27:07 +0300

    Testing 1

---
+++ src/units_conv.h
@@ -0,0 +1,13 @@
+//
+// Created by ilias on 6/18/23.
+//
+
+#ifndef AVANTI_UNITS_CONV_H
+#define AVANTI_UNITS_CONV_H
+
+#endif //AVANTI_UNITS_CONV_H
+
+//Helper function for using extra files.
+
+void  units_conv_init();
+void  units_conv_update();
